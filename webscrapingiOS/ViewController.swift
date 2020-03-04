70bfd973a76f80935225d43da8b138f78a5d2e8e
Tommy Dato <tom.dato@gmail.com>
2020-03-03 19:55:43 -0600

    Initial Commit

---
+++ webscrapingiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webscrapingiOS
+//
+//  Created by Thomas Dato on 3/3/20.
+//  Copyright © 2020 Tommy Dato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
